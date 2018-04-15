b0b864fd099af7fd796411368184b056596c63a7
Leander Jedamus <ljedamus@web.de>
2018-04-15 23:38:37 +0200

    minor changes

--- make.sh
+++ make.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 # erzeugt Mittwoch, 11. April 2018 17:18 (C) 2018 von Leander Jedamus
+# modifiziert Sonntag, 15. April 2018 23:36 von Leander Jedamus
 # modifiziert Mittwoch, 11. April 2018 17:21 von Leander Jedamus
 
-pdflatex --jobname=Dateiname "\def\settings {mode=label, labelsetup={labelsetup.conf}} \input{iX_Teil_III}" 
+pdflatex --jobname=inventar "\def\settings {mode=label, labelsetup={labelsetup.conf}} \input{iX_Teil_III}" 
 
 # vim:ai sw=2
 
