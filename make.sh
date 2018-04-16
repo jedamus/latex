d8db626f30d62000c1bf16c919a426ed4925f19f
Leander Jedamus <ljedamus@web.de>
2018-04-16 07:30:13 +0200

    added äöüÄÖÜ support, but not for ß

--- make.sh
+++ make.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 # erzeugt Mittwoch, 11. April 2018 17:18 (C) 2018 von Leander Jedamus
+# modifiziert Montag, 16. April 2018 07:21 von Leander Jedamus
 # modifiziert Sonntag, 15. April 2018 23:36 von Leander Jedamus
 # modifiziert Mittwoch, 11. April 2018 17:21 von Leander Jedamus
 
-pdflatex --jobname=inventar "\def\settings {mode=label, labelsetup={labelsetup.conf}} \input{iX_Teil_III}" 
+LATEX=pdflatex
+#LATEX=lualatex
+
+$LATEX --jobname=inventar "\def\settings {mode=label, labelsetup={labelsetup.conf}} \input{iX_Teil_III}" 
 
 # vim:ai sw=2
 
